fd175eff05ed70319274ce7047c31ebe557df402
Nick Driscoll <nick.a.mavros@gmail.com>
2018-07-02 15:16:39 +0000

    Replaced line with macro

--- radiography.c
+++ radiography.c
@@ -4,26 +4,31 @@
 
 int main(int argc, char *argv[])
 {
-	GtkBuilder      *builder; 
+	GtkBuilder      *builder;
 	GtkWidget       *window;
- 
+
 	gtk_init(&argc, &argv);
- 
+
 	builder = gtk_builder_new_from_file("radiography.glade");
- 
-	window = GTK_WIDGET(gtk_builder_get_object(builder, "xraywindow"));
+
+	window = GET_WIDGET(builder, "xraywindow");
 	gtk_builder_connect_signals(builder, NULL);
- 
+
 	g_object_unref(builder);
- 
-	gtk_widget_show(window);                
+
+	gtk_widget_show(window);
 	gtk_main();
- 
+
 	return 0;
 }
- 
+
 // called when window is closed
 void on_window_main_destroy()
 {
 	gtk_main_quit();
+}
+
+void jump_to_address(GtkButton* button, gpointer user_data)
+{
+
 }
