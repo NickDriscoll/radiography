24b3b02e1c377fbe99ac440937003e7d856f0bd5
Nick Driscoll <nick.a.mavros@gmail.com>
2018-07-01 20:14:51 -0400

    Added makefile

---
+++ Makefile
@@ -0,0 +1,7 @@
+CC=gcc
+
+all: radiography.o
+	$(CC) `pkg-config --cflags gtk+-3.0` $< -o radiography `pkg-config --libs gtk+-3.0` -export-dynamic $(CFLAGS)
+
+%.o: %.c
+	$(CC) `pkg-config --cflags gtk+-3.0` -c $< `pkg-config --libs gtk+-3.0` -export-dynamic $(CFLAGS)
--- radiography.c
+++ radiography.c
@@ -4,34 +4,26 @@
 
 int main(int argc, char *argv[])
 {
-    GtkBuilder      *builder; 
-    GtkWidget       *window;
+	GtkBuilder      *builder; 
+	GtkWidget       *window;
  
-    gtk_init(&argc, &argv);
+	gtk_init(&argc, &argv);
  
-    builder = gtk_builder_new_from_file("radiography.glade");
+	builder = gtk_builder_new_from_file("radiography.glade");
  
-    window = GTK_WIDGET(gtk_builder_get_object(builder, "xraywindow"));
-    gtk_builder_connect_signals(builder, NULL);
+	window = GTK_WIDGET(gtk_builder_get_object(builder, "xraywindow"));
+	gtk_builder_connect_signals(builder, NULL);
  
-    g_object_unref(builder);
+	g_object_unref(builder);
  
-    gtk_widget_show(window);                
-    gtk_main();
+	gtk_widget_show(window);                
+	gtk_main();
  
-    return 0;
+	return 0;
 }
  
 // called when window is closed
 void on_window_main_destroy()
 {
-    gtk_main_quit();
-}
-
-void jump_to_address()
-{
-    GtkWidget* child;
-    child = gtk_widget_new(GTK_TYPE_LABEL, "label", "Hello World", "xalign", 0.0, NULL);
-
-    gtk_list_box_prepend((GtkListBox*)GET_WIDGET(builder, "address_list"), child);
+	gtk_main_quit();
 }
