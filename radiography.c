0e0b138cbd3a6b7b6791729b1273ede11548b2a4
Nick Driscoll <nick.a.mavros@gmail.com>
2018-07-05 20:49:18 -0400

    Now it scrolls

--- radiography.c
+++ radiography.c
@@ -1,8 +1,10 @@
 #include <gtk/gtk.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #define GET_WIDGET(builder, x) GTK_WIDGET(gtk_builder_get_object(builder, x))
 #define GET_COLUMN(builder, x) GTK_TREE_VIEW_COLUMN(gtk_builder_get_object(builder, x))
+#define BUFFER_SIZE 1024
 
 void jump_to_address(GtkWidget* button, gpointer user_data);
 
@@ -10,47 +12,47 @@ int main(int argc, char *argv[])
 {
 	GtkBuilder*                 builder;
 	GtkWidget*                  window;
-    GtkWidget*                  jump_button;
-    GtkWidget*                  address_list_view;
-    GtkTreeViewColumn*          address_column;
-    GtkTreeViewColumn*          value_column;
-    GtkListStore*               address_list_store;
-    GtkCellRenderer*            address_renderer;
-    GtkCellRenderer*            value_renderer;
+	GtkWidget*                  jump_button;
+	GtkWidget*                  address_list_view;
+	GtkTreeViewColumn*          address_column;
+	GtkTreeViewColumn*          value_column;
+	GtkListStore*               address_list_store;
+	GtkCellRenderer*            address_renderer;
+	GtkCellRenderer*            value_renderer;
 
 	gtk_init(&argc, &argv);
 
 	builder = gtk_builder_new_from_file("radiography.glade");
 
 	window = GET_WIDGET(builder, "xraywindow");
-    jump_button = GET_WIDGET(builder, "jump_button");
-    address_list_view = GET_WIDGET(builder, "address_list_view");
-    address_column = GET_COLUMN(builder, "address_column");
-    value_column = GET_COLUMN(builder, "value_column");
+	jump_button = GET_WIDGET(builder, "jump_button");
+	address_list_view = GET_WIDGET(builder, "address_list_view");
+	address_column = GET_COLUMN(builder, "address_column");
+	value_column = GET_COLUMN(builder, "value_column");
 
-    address_list_store = gtk_list_store_new(2, G_TYPE_STRING, G_TYPE_STRING);
+	address_list_store = gtk_list_store_new(2, G_TYPE_STRING, G_TYPE_STRING);
 
-    /* Connect signals with callback functions */
+	/* Connect signals with callback functions */
 	gtk_builder_connect_signals(builder, NULL);
-    g_signal_connect(jump_button, "clicked", G_CALLBACK(jump_to_address), address_list_store);
-    
-    /* Connect the list view to the list model */
-    gtk_tree_view_set_model(GTK_TREE_VIEW(address_list_view), GTK_TREE_MODEL(address_list_store));
+	g_signal_connect(jump_button, "clicked", G_CALLBACK(jump_to_address), address_list_store);
 
-    /* Create the cell renderers*/
-    address_renderer = gtk_cell_renderer_text_new();
-    value_renderer = gtk_cell_renderer_text_new();
+	/* Connect the list view to the list model */
+	gtk_tree_view_set_model(GTK_TREE_VIEW(address_list_view), GTK_TREE_MODEL(address_list_store));
 
-    /* Connect the columns to their renderers */
-    gtk_tree_view_column_pack_start(address_column, address_renderer, TRUE);
-    gtk_tree_view_column_pack_start(value_column, value_renderer, TRUE);
+	/* Create the cell renderers*/
+	address_renderer = gtk_cell_renderer_text_new();
+	value_renderer = gtk_cell_renderer_text_new();
 
-    /* Connect renderers to the list store */
-    gtk_tree_view_column_add_attribute(address_column, address_renderer, "text", 0);
-    gtk_tree_view_column_add_attribute(value_column, value_renderer, "text", 1);
+	/* Connect the columns to their renderers */
+	gtk_tree_view_column_pack_start(address_column, address_renderer, TRUE);
+	gtk_tree_view_column_pack_start(value_column, value_renderer, TRUE);
+
+	/* Connect renderers to the list store */
+	gtk_tree_view_column_add_attribute(address_column, address_renderer, "text", 0);
+	gtk_tree_view_column_add_attribute(value_column, value_renderer, "text", 1);
 
 	g_object_unref(builder);
-    g_object_unref(address_list_store);
+	g_object_unref(address_list_store);
 
 	gtk_widget_show(window);
 	gtk_main();
@@ -66,30 +68,34 @@ void on_window_main_destroy()
 
 void on_pid_text_entry(GtkEntry *entry, gchar* string, gpointer user_data)
 {
-    if (string[strlen(string) - 1] < 48 || string[strlen(string) - 1] > 57)
-        string[strlen(string) - 1] = '\0';
+	if (string[strlen(string) - 1] < 48 || string[strlen(string) - 1] > 57)
+	    string[strlen(string) - 1] = '\0';
 }
 
 void attach_to_process(GtkWidget* button, gpointer user_data)
 {
-    GtkEntry* pid_entry = (GtkEntry*)user_data;
-    FILE* f;
-    char* file_path;
+	GtkEntry* pid_entry = (GtkEntry*)user_data;
+	FILE* f;
+	char* file_path;
+	char proc_name[BUFFER_SIZE];
 
-    /* Make*/
+	/* Make file_path string */
+	sprintf(file_path, "/proc/%i/comm", atoi(gtk_entry_get_text(pid_entry)));
+	f = fopen(file_path, "r");
+	fread(proc_name, BUFFER_SIZE, 1, f);
+	fclose(f);
 
-    sprintf(file_path, "/proc/%i/comm", atoi(gtk_entry_get_text(pid_entry)));
-    //f = fopen(file_path, "r");
-
+	/* Set window title */
+	
 }
 
 void jump_to_address(GtkWidget* button, gpointer user_data)
 {
-    GtkListStore* store = (GtkListStore*)user_data;
-    GtkTreeIter iter;
-    gtk_list_store_append(store, &iter);
-    gtk_list_store_set(store, &iter,
-                       0, "Test",
-                       1, "Text",
-                       -1);
+	GtkListStore* store = (GtkListStore*)user_data;
+	GtkTreeIter iter;
+	gtk_list_store_append(store, &iter);
+	gtk_list_store_set(store, &iter,
+	                   0, "Test",
+	                   1, "Text",
+	                   -1);
 }
