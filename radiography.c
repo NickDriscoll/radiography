44ef72768e85d68f5995a78b955f8151005c73ee
Nick Driscoll <nick.a.mavros@gmail.com>
2018-07-06 10:05:30 -0400

    Source clean up

--- radiography.c
+++ radiography.c
@@ -14,23 +14,23 @@ void attach_to_process(GtkWidget* button, gpointer user_data);
 
 typedef struct
 {
-    GtkWindow* window;
-    GtkEntry* box;
-    pid_t* pid;
+	GtkWindow* window;
+	GtkEntry* box;
+	pid_t* pid;
 } attach_s;
 
 int main(int argc, char *argv[])
 {
-    /*Actual program variables */
-    pid_t victim_pid;
+	/*Actual program variables */
+	pid_t victim_pid;
 
-    /* GUI variables */
+	/* GUI variables */
 	GtkBuilder*                 builder;
 	GtkWindow*                  window;
 	GtkWidget*                  jump_button;
-    GtkWidget*                  pid_button;
+	GtkWidget*                  pid_button;
 	GtkWidget*                  address_list_view;
-    GtkEntry*                   pid_entry;
+	GtkEntry*                   pid_entry;
 	GtkTreeViewColumn*          address_column;
 	GtkTreeViewColumn*          value_column;
 	GtkListStore*               address_list_store;
@@ -42,25 +42,25 @@ int main(int argc, char *argv[])
 	builder = gtk_builder_new_from_file("radiography.glade");
 
 	window = GET_WINDOW(builder, "xraywindow");
-    pid_entry = GET_ENTRY(builder, "pid_entry");
+	pid_entry = GET_ENTRY(builder, "pid_entry");
 	jump_button = GET_WIDGET(builder, "jump_button");
-    pid_button = GET_WIDGET(builder, "pid_button");
+	pid_button = GET_WIDGET(builder, "pid_button");
 	address_list_view = GET_WIDGET(builder, "address_list_view");
 	address_column = GET_COLUMN(builder, "address_column");
 	value_column = GET_COLUMN(builder, "value_column");
 
 	address_list_store = gtk_list_store_new(2, G_TYPE_STRING, G_TYPE_STRING);
 
-    /* Create callback args */
-    attach_s* pid_struct = malloc(sizeof(attach_s));
-    pid_struct->window = window;
-    pid_struct->box = pid_entry;
-    pid_struct->pid = &victim_pid;
-    
+	/* Create callback args */
+	attach_s* pid_struct = malloc(sizeof(attach_s));
+	pid_struct->window = window;
+	pid_struct->box = pid_entry;
+	pid_struct->pid = &victim_pid;
+
 	/* Connect signals with callback functions */
 	gtk_builder_connect_signals(builder, NULL);
 	g_signal_connect(jump_button, "clicked", G_CALLBACK(jump_to_address), address_list_store);
-    g_signal_connect(pid_button, "clicked", G_CALLBACK(attach_to_process), pid_struct);
+	g_signal_connect(pid_button, "clicked", G_CALLBACK(attach_to_process), pid_struct);
 
 	/* Connect the list view to the list model */
 	gtk_tree_view_set_model(GTK_TREE_VIEW(address_list_view), GTK_TREE_MODEL(address_list_store));
@@ -95,7 +95,7 @@ void on_window_main_destroy()
 void on_pid_text_entry(GtkEntry *entry, gchar* string, gpointer user_data)
 {
 	if (string[strlen(string) - 1] < 48 || string[strlen(string) - 1] > 57)
-	    string[strlen(string) - 1] = '\0';
+		string[strlen(string) - 1] = '\0';
 }
 
 void attach_to_process(GtkWidget* button, gpointer user_data)
@@ -103,35 +103,35 @@ void attach_to_process(GtkWidget* button, gpointer user_data)
 	attach_s* pid_struct = (attach_s*)user_data;
 	FILE* f;
 	char file_path[BUFFER_SIZE] = {0};
-    char new_window_title[BUFFER_SIZE] = {0};
+	char new_window_title[BUFFER_SIZE] = {0};
 	char proc_name[BUFFER_SIZE] = {0};
-    pid_t new_pid;
+	pid_t new_pid;
 
-    new_pid = atoi(gtk_entry_get_text(pid_struct->box));
+	new_pid = atoi(gtk_entry_get_text(pid_struct->box));
 
 	/* Make file_path string */
 	sprintf(file_path, "/proc/%i/comm", new_pid);
-	
 
-    f = fopen(file_path, "r");
-    if (f == NULL)
-    {
-        gtk_window_set_title(pid_struct->window, "ERROR ATTACHING TO PROCESS");
-        return;
-    }
+
+	f = fopen(file_path, "r");
+	if (f == NULL)
+	{
+		gtk_window_set_title(pid_struct->window, "ERROR ATTACHING TO PROCESS");
+		return;
+	}
 
 	fread(proc_name, BUFFER_SIZE, 1, f);
 	fclose(f);
 
-    /* Remove linebreak */
-    proc_name[strlen(proc_name) - 1] = '\0';
+	/* Remove linebreak */
+	proc_name[strlen(proc_name) - 1] = '\0';
 
 	/* Set window title */
-    sprintf(new_window_title, "Attached to: %s", proc_name);
+	sprintf(new_window_title, "Attached to: %s", proc_name);
 	gtk_window_set_title(pid_struct->window, new_window_title);
 
-    /* Store pid */
-    *pid_struct->pid = new_pid;
+	/* Store pid */
+	*pid_struct->pid = new_pid;
 }
 
 void jump_to_address(GtkWidget* button, gpointer user_data)
