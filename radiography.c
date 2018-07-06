230d555fcf2181b41d9cd023c049dbce090b22d9
Nick Driscoll <nick.a.mavros@gmail.com>
2018-07-06 11:17:58 -0400

    Began implementing jump_to_address

--- radiography.c
+++ radiography.c
@@ -1,41 +1,54 @@
+#define _GNU_SOURCE
 #include <gtk/gtk.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/uio.h>
 
 #define GET_WIDGET(builder, x) GTK_WIDGET(gtk_builder_get_object(builder, x))
 #define GET_WINDOW(builder ,x) GTK_WINDOW(gtk_builder_get_object(builder, x))
 #define GET_ENTRY(builder, x) GTK_ENTRY(gtk_builder_get_object(builder, x))
 #define GET_COLUMN(builder, x) GTK_TREE_VIEW_COLUMN(gtk_builder_get_object(builder, x))
+
 #define BUFFER_SIZE 1024
 
 void jump_to_address(GtkWidget* button, gpointer user_data);
 void attach_to_process(GtkWidget* button, gpointer user_data);
 
+typedef unsigned char byte;
+
 typedef struct
 {
-	GtkWindow* window;
-	GtkEntry* box;
-	pid_t* pid;
+	GtkWindow*	window;
+	GtkEntry*	box;
+	pid_t*		pid;
 } attach_s;
+
+typedef struct
+{
+	GtkListStore*	list_store;
+	GtkEntry*	entry;
+	pid_t*		pid;
+} read_s;
 
 int main(int argc, char *argv[])
 {
 	/*Actual program variables */
-	pid_t victim_pid;
+	pid_t target_pid;
 
 	/* GUI variables */
-	GtkBuilder*                 builder;
-	GtkWindow*                  window;
-	GtkWidget*                  jump_button;
-	GtkWidget*                  pid_button;
-	GtkWidget*                  address_list_view;
-	GtkEntry*                   pid_entry;
-	GtkTreeViewColumn*          address_column;
-	GtkTreeViewColumn*          value_column;
-	GtkListStore*               address_list_store;
-	GtkCellRenderer*            address_renderer;
-	GtkCellRenderer*            value_renderer;
+	GtkBuilder*			builder;
+	GtkWindow*			window;
+	GtkWidget*			jump_button;
+	GtkWidget*			pid_button;
+	GtkWidget*			address_list_view;
+	GtkEntry*			pid_entry;
+	GtkEntry*			address_entry;
+	GtkTreeViewColumn*		address_column;
+	GtkTreeViewColumn*		value_column;
+	GtkListStore*			address_list_store;
+	GtkCellRenderer*		address_renderer;
+	GtkCellRenderer*		value_renderer;
 
 	gtk_init(&argc, &argv);
 
@@ -43,6 +56,7 @@ int main(int argc, char *argv[])
 
 	window = GET_WINDOW(builder, "xraywindow");
 	pid_entry = GET_ENTRY(builder, "pid_entry");
+	address_entry = GET_ENTRY(builder, "address_entry");
 	jump_button = GET_WIDGET(builder, "jump_button");
 	pid_button = GET_WIDGET(builder, "pid_button");
 	address_list_view = GET_WIDGET(builder, "address_list_view");
@@ -55,11 +69,16 @@ int main(int argc, char *argv[])
 	attach_s* pid_struct = malloc(sizeof(attach_s));
 	pid_struct->window = window;
 	pid_struct->box = pid_entry;
-	pid_struct->pid = &victim_pid;
+	pid_struct->pid = &target_pid;
+
+	read_s* read_struct = malloc(sizeof(read_s));
+	read_struct->list_store = address_list_store;
+	read_struct->entry = address_entry;
+	read_struct->pid = &target_pid;
 
 	/* Connect signals with callback functions */
 	gtk_builder_connect_signals(builder, NULL);
-	g_signal_connect(jump_button, "clicked", G_CALLBACK(jump_to_address), address_list_store);
+	g_signal_connect(jump_button, "clicked", G_CALLBACK(jump_to_address), read_struct);
 	g_signal_connect(pid_button, "clicked", G_CALLBACK(attach_to_process), pid_struct);
 
 	/* Connect the list view to the list model */
@@ -136,11 +155,36 @@ void attach_to_process(GtkWidget* button, gpointer user_data)
 
 void jump_to_address(GtkWidget* button, gpointer user_data)
 {
-	GtkListStore* store = (GtkListStore*)user_data;
-	GtkTreeIter iter;
+	GtkListStore*		store;
+	GtkTreeIter		iter;
+	pid_t			target_pid;
+	struct iovec* local_vec;
+	struct iovec* remote_vec;
+
+	read_s* args = user_data;
+	store = args->list_store;
+	target_pid = *args->pid;
+	local_vec = malloc(sizeof(struct iovec));
+	remote_vec = malloc(sizeof(struct iovec));
+
+	/* Set up local vector */
+	local_vec->iov_len = 100;
+	local_vec->iov_base = malloc(local_vec->iov_len);
+
+	/* Set up remote vector */
+	remote_vec->iov_len = 100;
+	remote_vec->iov_base = (void*)atoll(gtk_entry_get_text(args->entry));
+
+	/* Read memory from process */
+	process_vm_readv(target_pid, local_vec, 1, remote_vec, 1, 0);
+
 	gtk_list_store_append(store, &iter);
 	gtk_list_store_set(store, &iter,
 	                   0, "Test",
 	                   1, "Text",
 	                   -1);
+
+	free(local_vec->iov_base);
+	free(local_vec);
+	free(remote_vec);
 }
