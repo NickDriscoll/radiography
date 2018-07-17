74ba6447f7b492861b218156dfa05ea9a5a5d8f5
Nick Driscoll <nick.a.mavros@gmail.com>
2018-07-17 15:35:06 -0400

    Fixed my inability to do math

--- radiography.c
+++ radiography.c
@@ -233,7 +233,7 @@ void jump_to_address(GtkWidget* button, gpointer user_data)
 
 	/* Set up remote vector */
 	remote_vec->iov_len = NUMBER_OF_BYTES_TO_READ;
-	remote_vec->iov_base = (void*)(strtoll(gtk_entry_get_text(args->entry), NULL, 16) / 2);
+	remote_vec->iov_base = (void*)(strtoll(gtk_entry_get_text(args->entry), NULL, 16) - (NUMBER_OF_BYTES_TO_READ / 2));
 
 	/* Read memory from process */
 	if (process_vm_readv(target_pid, local_vec, 1, remote_vec, 1, 0) == -1)
